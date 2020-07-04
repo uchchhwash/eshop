cfec26c4962ac13545116f9e496764d8673aacd5
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-07-04 18:10:44 +0600

    file experiment successful

--- models/product.js
+++ models/product.js
@@ -1,33 +1,33 @@
 const path = require("path");
 const fs = require("fs");
+const p = path.join(path.dirname(require.main.filename), "data", "products.json");
 
+const getProductFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    });
+}
 module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
     save() {
-        const p = path.join(path.dirname(require.main.filename), "data", "products.json");
-
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
             });
-        });
+
+        })
     }
 
     static fetchAll(cb) {
-        const p = path.join(path.dirname(require.main.filename), "data", "products.json");
-        const test = fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
-        return test;
+        getProductFromFile(products => {
+            cb(products);
+        })
     }
 }
