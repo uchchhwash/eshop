7b9db188b29063f06b9d3200f6d4567a6cbc9330
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-07-04 21:27:51 +0600

    product detail page implement

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
 const errorController = require("./controllers/error");
+
 const app = express();
 
-app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
-app.use("/shop", shopRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000, () => {
-    console.log("server is listening on port: 3000")
-})
+app.listen(3000);
