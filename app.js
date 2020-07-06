dc67e0886f48c86609119117d43f79823040677e
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-07-06 18:12:55 +0600

    flash error messages for user feedback

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI =
-    'mongodb://localhost:27017/eshop';
+const MONGODB_URI = 'mongodb://localhost:27017/eshop';
 
 const app = express();
 const store = new MongoDBStore({
@@ -38,6 +38,7 @@ app.use(
     })
 );
 app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
     if (!req.session.user) {
