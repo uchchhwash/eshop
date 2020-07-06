20f72a2c34247f7aed1a71d6b24889d7a89d8a33
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-07-06 14:29:41 +0600

    route middleware protection add

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-    .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+    .connect(MONGODB_URI)
     .then(result => {
         app.listen(3000);
     })
