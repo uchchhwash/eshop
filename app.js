1073bf05dc88cc77638afc1d2c0be2ef04cdbeee
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-07-06 15:24:39 +0600

    csrf implementation

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI = 'mongodb://localhost:27017/eshop';
+const MONGODB_URI =
+    'mongodb://localhost:27017/eshop';
 
 const app = express();
 const store = new MongoDBStore({
     uri: MONGODB_URI,
     collection: 'sessions'
 });
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -34,6 +37,7 @@ app.use(
         store: store
     })
 );
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
     if (!req.session.user) {
@@ -45,6 +49,12 @@ app.use((req, res, next) => {
             next();
         })
         .catch(err => console.log(err));
+});
+
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
 });
 
 app.use('/admin', adminRoutes);
