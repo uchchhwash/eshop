6177272990b1377fed695115654f9cc5207a179c
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-07-06 14:16:01 +0600

    session & basic authentication

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URL = 'mongodb://localhost:27017/eshop';
+const MONGODB_URI = 'mongodb://localhost:27017/eshop';
 
 const app = express();
 const store = new MongoDBStore({
-    uri: MONGODB_URL,
+    uri: MONGODB_URI,
     collection: 'sessions'
 });
 
@@ -28,14 +28,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
     session({
-        secret: 'tyetdgdtehdsbkvj',
+        secret: 'my secret',
         resave: false,
         saveUninitialized: false,
         store: store
-    }));
+    })
+);
 
 app.use((req, res, next) => {
-    User.findById('5f01e65c6272a66374ffa5e7')
+    if (!req.session.user) {
+        return next();
+    }
+    User.findById(req.session.user._id)
         .then(user => {
             req.user = user;
             next();
@@ -50,20 +54,8 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-    .connect(MONGODB_URL)
+    .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
     .then(result => {
-        User.findOne().then(user => {
-            if (!user) {
-                const user = new User({
-                    name: 'Max',
-                    email: 'max@test.com',
-                    cart: {
-                        items: []
-                    }
-                });
-                user.save();
-            }
-        });
         app.listen(3000);
     })
     .catch(err => {
