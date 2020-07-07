5f15d67268b9a18af917e985df6b8f6a6f1c8399
Uchchhwash Chakraborty <uchchhwash.git@gmail.com>
2020-07-07 13:20:36 +0600

    server side validations

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI = 'mongodb://localhost:27017/eshop';
-
 const app = express();
 const store = new MongoDBStore({
     uri: MONGODB_URI,
